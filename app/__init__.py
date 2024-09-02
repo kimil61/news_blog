58d1e9b08633369c78b92e4079124c189ac18427
Jason Kim <json@Jasons-MacBook-Pro.local>
2024-09-02 18:24:52 +0800

    sqlite ==> mysql 로 데이터베이스 바꿈

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,3 @@
-#/app/__init__.py
 import logging
 from logging.handlers import RotatingFileHandler
 import os
@@ -10,6 +9,11 @@ from config import Config
 from flask_wtf.csrf import CSRFProtect
 from flask_caching import Cache
 from dotenv import load_dotenv
+import pymysql
+
+# .env 파일 로드
+load_dotenv()
+pymysql.install_as_MySQLdb()
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -18,16 +22,15 @@ login_manager.login_view = 'auth.login'
 csrf = CSRFProtect()
 cache = Cache(config={'CACHE_TYPE': 'simple'})
 
-# .env 파일 로드
-load_dotenv()
-
 ## 개발환경에서만, 구글 로그인을 위한 설정. https 를 적용하면 이거 삭제해야 함.
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(Config)
+
+    # Config 클래스 초기화 및 적용
+    Config.init_app(app)
 
     db.init_app(app)
     migrate.init_app(app, db)
@@ -35,8 +38,7 @@ def create_app():
     csrf.init_app(app)
     cache.init_app(app)
 
-    from app.views import auth, user, post, tag ,errors,comment
-    # app.register_blueprint(main.bp)
+    from app.views import auth, user, post, tag, errors, comment
     app.register_blueprint(auth.bp)
     app.register_blueprint(user.bp)
     app.register_blueprint(post.bp)
--- config.py
+++ config.py
@@ -1,9 +1,19 @@
 import os
 
+
 class Config:
     WTF_CSRF_ENABLED = True
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess-aha'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(os.path.abspath(os.path.dirname(__file__)), 'app.db')
+    SECRET_KEY = 'you-will-never-guess-aha'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    
+
+    @classmethod
+    def init_app(cls, app):
+        cls.SECRET_KEY = os.environ.get('SECRET_KEY') or cls.SECRET_KEY
+        cls.MYSQL_HOST = os.environ.get('MYSQL_HOST', 'localhost')
+        cls.MYSQL_USER = os.environ.get('MYSQL_USER', 'root')
+        cls.MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD', '')
+        cls.MYSQL_DB = os.environ.get('MYSQL_DB', 'newsblog')
+
+        cls.SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{cls.MYSQL_USER}:{cls.MYSQL_PASSWORD}@{cls.MYSQL_HOST}/{cls.MYSQL_DB}"
+
+        app.config.from_object(cls)
