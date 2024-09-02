813f6cb1346f7249baedad686940380090091d56
Jason Kim <json@Jasons-MacBook-Pro.local>
2024-09-02 10:29:15 +0800

    good

---
+++ app/models/post.py
@@ -0,0 +1,11 @@
+from app import db
+from datetime import datetime
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f'<Post {self.title}>'
---
+++ app/views/main.py
@@ -0,0 +1,26 @@
+from flask import Blueprint, render_template, request, redirect, url_for
+from app import db
+from app.models.post import Post
+
+bp = Blueprint('main', __name__)
+
+@bp.route('/')
+def index():
+    posts = Post.query.order_by(Post.created_at.desc()).all()
+    return render_template('index.html', posts=posts)
+
+@bp.route('/post/<int:post_id>')
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', post=post)
+
+@bp.route('/create', methods=['GET', 'POST'])
+def create_post():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+        post = Post(title=title, content=content)
+        db.session.add(post)
+        db.session.commit()
+        return redirect(url_for('main.index'))
+    return render_template('create_post.html')
